00a8b987824ab62c162c68b8279071d96cd82376
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-11-05 05:25:13 -0700

    Add main.h file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void execute(char **args);
+char **parse(char *input);
+char *read_line();
+
+#endif
