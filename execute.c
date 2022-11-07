5906ae3f86585158af5dc36c5eb4606ba37d1f72
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-11-07 07:32:40 -0800

    Factorize execute function

--- execute.c
+++ execute.c
@@ -1,43 +1,33 @@
 #include "main.h"
 
+#undef DELIMITER
+
 /**
- * execute - Executes shell commands
- * @args: Arguments to be executed
+ * execute - process command and execute process
+ * @data: a pointer to the struct of data
+ *
+ * Return: (Success) a positive number
+ * ------- (Fail) a negative number
  */
 
-void execute(char **args)
+int execute(sh_t *data)
 {
 	/* Execute the command using execve or execvp function */
-	if (_strcmp(args[0], "cd") == 0)
+
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == 0)
 	{
-		if (chdir(args[1]) == -1)
-			perror("Error");
+		signal(SIGINT, SIG_DFL);
+		if (execve(data->cmd, data->args, environ) < 0)
+		data->error_msg = _strdup("command not found\n");
+			return (FAIL);
 	}
-	else if (_strcmp(args[0], "env") == 0)
-	{
-		char *dup = getenv("PATH");
-		char *s = dup;
-		char *p = NULL;
-		do {
-			p = strtok(s, ":");
-			if (p != NULL)
-				p[0] = 0;
-			printf("%s\n", s);
-			s = p + 1;
-		} while (p != NULL);
-	}
-	else if (_strcmp(args[0], "exit") == 0)
-		exit(0);
 	else
 	{
-		if (fork() == 0)/* checks any current running process */
-		{
-			int status_code = execve(args[0], args, NULL); /* Change execve to execvp(args[0], args) envp|NULL excluded */
-
-			if (status_code == -1)/* checks valid command */
-				perror("Error: Execution error.");
-		}
-		else
-			wait(NULL); /* wait for child process to finish, to avoid zombi state | orphan state */
+		waitpid(pid, &status, WUNTRACED);
 	}
+	return (0);
 }
