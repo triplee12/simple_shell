546231fb367c839ef4f34f66df9116120e1fc21c
Ejie Emmanuel Ebuka <tripleeoliver@gmail.com>
2022-11-07 04:35:04 -0800

    Add more prototypes and structs

--- main.h
+++ main.h
@@ -8,9 +8,22 @@
 #include <stdbool.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <dirent.h>
+
+typedef struct aliases {
+    char *alias_name;
+    char *real_name;
+} alias;
+
+typedef struct my_builtins{
+    char *name;
+    int (*func)(void);
+} my_builtins;
 
 void execute(char **args);
 char **parse(char *input);
 char *read_line();
+int _strcmp(char *str1, char *str2);
+int _strcpy(char *str1, char *str2, int s);
 
 #endif
